55becc70f06af076eb2019f503fc8af049296232
Antoine <antoine.coste@e.rascol.net>
2024-04-16 19:34:54 +0200

    Tentative de fonctions de sauvegarde/charge
    ça fonctionne parfait sauf que les listes qui sont chargées à partir de csv marchent pas et sont prises comme des chaînes de caractères

--- definitions.py
+++ definitions.py
@@ -19,7 +19,8 @@ class Entity :
         self.pos[1] -= amount
 
 class Arme :
-    def __init__(self,degat):
+    def __init__(self,nom,degat):
+        self.nom = nom
         self.degat = degat
 
 class Personnage(Entity):
@@ -46,33 +47,31 @@ class Ennemis:
     def retirer(self):
         pass # à coder plus tard, il faut trouver un moyen de trouver un élément de la liste par son nom ou un truc comme ça
     def sauvegarder(self, emplacementSave):
-        with open("Sauvegardes/"+emplacementSave+".csv", 'w') as csvfile : # On ouvre/crée le fichier csv, puis on le manipule avec la bibliothèque csv.
+        with open("Sauvegardes/"+emplacementSave+".csv", 'w', newline='') as csvfile : # On ouvre/crée le fichier csv, puis on le manipule avec la bibliothèque csv.
             sauvegarde = csv.writer(csvfile)
-            listeAttributs = []
+
 
             for i in range(len(self.ennemisList)):
-                listeAttributs.append([])
+                listeAttributs = []
                 for attribut in self.ennemisList[i].__dict__.values() :
                     if isinstance(attribut, Arme):
-                        listeAttributs[i].append(attribut.degat)
+                        listeAttributs.append([attribut.nom, attribut.degat])
                     else:
-                        listeAttributs[i].append(attribut)
-            sauvegarde.writerow(listeAttributs)  # Si on met cette ligne dans la boucle principale, ça semble bien mais y'a des champs vides et ça plante quand il faut charger (peut être retirer ces champs vides ?)
+                        listeAttributs.append(attribut)
+                sauvegarde.writerow(listeAttributs)
 
 
 
     def charger(self, emplacementSave):
-        with open("Sauvegardes/"+emplacementSave+".csv") as csvfile:
+        with open("Sauvegardes/"+emplacementSave+".csv", "r") as csvfile:
             charge = csv.reader(csvfile)
             self.ennemisList = []
-            ennemiActuel = []
 
             for perso in charge :
-                ennemiActuel = perso
-                print(ennemiActuel[0]) # Semble afficher une liste ?
-                self.ennemisList.append(Personnage(ennemiActuel[0][0],ennemiActuel[0][1],ennemiActuel[0][2] ,ennemiActuel[0][3],ennemiActuel[0][5],ennemiActuel[0][6])) # Théoriquement ça marche, mais il ne semble pas comprendre que c'est une liste et le prend comme une chaîne de caractères
-                ennemiActuel = []
+                print(perso[6])
+                if perso[6] != '':
+                    self.ennemisList.append(Personnage(perso[0],int(perso[1]),int(perso[2]),int(perso[3]),perso[4],perso[5],Arme(perso[6][0],int(perso[6][1])))) 
+                else:
+                    self.ennemisList.append(Personnage(perso[0],int(perso[1]),int(perso[2]),int(perso[3]),perso[4],perso[5],None)) 
 
-                
-# Documentation du module csv :
-# https://docs.python.org/3/library/csv.html
+
