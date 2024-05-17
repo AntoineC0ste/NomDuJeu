9ef0b8d9e9985289e53877eef836395336c80eb4
adamtche <adam.timirbulatov@e.rascol.net>
2024-05-17 17:19:19 +0200

    Nouvelle_Map+Tileset

--- Carte/Default.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Default" tilewidth="32" tileheight="32" tilecount="180" columns="12">
- <image source="../../../../Downloads/image.png" width="384" height="480"/>
-</tileset>
---
+++ Carte/RPG_Nature_Tilset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="RPG_Nature_Tilset" tilewidth="32" tileheight="32" tilecount="180" columns="20">
+ <image source="RPG_Nature_Tilset.png" trans="000000" width="641" height="288"/>
+</tileset>
