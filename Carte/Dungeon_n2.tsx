acc0e0ed68bae3b950219a675ddaebd29e1a509d
adamtche <adam.timirbulatov@e.rascol.net>
2024-05-18 16:44:50 +0200

    Creation premier dongeon et debut map

---
+++ Carte/Dungeon_n2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Dungeon_n2" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="Dungeon_n2.png" trans="000000" width="1024" height="512"/>
+</tileset>
